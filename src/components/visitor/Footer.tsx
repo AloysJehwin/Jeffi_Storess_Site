507ca483a29d7aadc1fff2f032165153e7552735
Aloys Jehwin <124761596+AloysJehwin@users.noreply.github.com>
2026-04-27 13:57:42 +0530

    fix: use pb-20 on mobile footer to reliably clear browser bottom bar (#51)
    Co-authored-by: Aloys Jehwin <aloys.jehwin@sap.com>

--- src/components/visitor/Footer.tsx
+++ src/components/visitor/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer className="bg-secondary-800 text-white">
-      <div className="container mx-auto px-4 pt-12 pb-12 pb-safe-4">
+      <div className="container mx-auto px-4 pt-12 pb-20 md:pb-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* About Column */}
           <div>
